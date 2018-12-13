c693ecd52ac7d6ccdb401c8715ee376749e8fccc
Ayush <38552529+Ayush404@users.noreply.github.com>
2018-12-13 14:27:29 +0530

    Add files via upload

---
+++ Website-Scanner/website.py
@@ -0,0 +1,7 @@
+websites = [('facebook', 'https://www.facebook.com'),
+            ('gmail', 'https://www.gmail.com'),
+            
+            ('reddit', 'https://www.reddit.com'),
+            ('linkedin', 'https://www.linkedin.com'),
+
+        ]
